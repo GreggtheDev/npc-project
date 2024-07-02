7ac78870584cf32a4844e450753ab733ff76ddf5
GreggtheDev <greggthedev@gmail.com>
2024-07-02 13:32:35 -0400

    npm project

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello, world!");// console log returns 'Hello World'
+
+const moment = require('moment');// import moment library
+
+console.log(`Current time: ${moment().format('h:mm:ss a')}`);
+// console log returns 'Current time: 11:00:00 am'
